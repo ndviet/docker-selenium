799f6f8b73343d4719b68e2d09f2bb03bbb606a8
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-08-12 16:33:39 +0000

    fix: Conflict CLI options from SE_OPTS and corresponding env variables
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- EventBus/start-selenium-grid-eventbus.sh
+++ EventBus/start-selenium-grid-eventbus.sh
@@ -5,6 +5,23 @@ set -e
 
 echo "Starting Selenium Grid EventBus..."
 
+function append_se_opts() {
+  local option="${1}"
+  local value="${2:-""}"
+  local log_message="${3:-true}"
+  if [[ "${SE_OPTS}" != *"${option}"* ]]; then
+    if [ "${log_message}" = "true" ]; then
+      echo "Appending Selenium option: ${option} ${value}"
+    fi
+    SE_OPTS="${SE_OPTS} ${option}"
+    if [ ! -z "${value}" ]; then
+      SE_OPTS="${SE_OPTS} ${value}"
+    fi
+  else
+    echo "Selenium option: ${option} already set in env variable SE_OPTS. Ignore new option: ${option} ${value}"
+  fi
+}
+
 if [ ! -z "$SE_EVENT_BUS_HOST" ]; then
   echo "Using SE_EVENT_BUS_HOST: ${SE_EVENT_BUS_HOST}"
   HOST_CONFIG="--host ${SE_EVENT_BUS_HOST}"
@@ -15,28 +32,20 @@ if [ ! -z "$SE_EVENT_BUS_PORT" ]; then
   PORT_CONFIG="--port ${SE_EVENT_BUS_PORT}"
 fi
 
-if [ ! -z "$SE_OPTS" ]; then
-  echo "Appending Selenium options: ${SE_OPTS}"
-fi
-
 if [ ! -z "$SE_LOG_LEVEL" ]; then
-  echo "Appending Selenium options: --log-level ${SE_LOG_LEVEL}"
-  SE_OPTS="$SE_OPTS --log-level ${SE_LOG_LEVEL}"
+  append_se_opts "--log-level" "${SE_LOG_LEVEL}"
 fi
 
 if [ ! -z "$SE_HTTP_LOGS" ]; then
-  echo "Appending Selenium options: --http-logs ${SE_HTTP_LOGS}"
-  SE_OPTS="$SE_OPTS --http-logs ${SE_HTTP_LOGS}"
+  append_se_opts "--http-logs" "${SE_HTTP_LOGS}"
 fi
 
 if [ ! -z "$SE_STRUCTURED_LOGS" ]; then
-  echo "Appending Selenium options: --structured-logs ${SE_STRUCTURED_LOGS}"
-  SE_OPTS="$SE_OPTS --structured-logs ${SE_STRUCTURED_LOGS}"
+  append_se_opts "--structured-logs" "${SE_STRUCTURED_LOGS}"
 fi
 
 if [ ! -z "$SE_EXTERNAL_URL" ]; then
-  echo "Appending Selenium options: --external-url ${SE_EXTERNAL_URL}"
-  SE_OPTS="$SE_OPTS --external-url ${SE_EXTERNAL_URL}"
+  append_se_opts "--external-url" "${SE_EXTERNAL_URL}"
 fi
 
 if [ "${SE_ENABLE_TLS}" = "true" ]; then
@@ -57,12 +66,10 @@ if [ "${SE_ENABLE_TLS}" = "true" ]; then
   SE_JAVA_OPTS="$SE_JAVA_OPTS -Djdk.internal.httpclient.disableHostnameVerification=${SE_JAVA_DISABLE_HOSTNAME_VERIFICATION}"
   # Configure certificate and private key for component communication
   if [ ! -z "$SE_HTTPS_CERTIFICATE" ]; then
-    echo "Appending Selenium options: --https-certificate ${SE_HTTPS_CERTIFICATE}"
-    SE_OPTS="$SE_OPTS --https-certificate ${SE_HTTPS_CERTIFICATE}"
+    append_se_opts "--https-certificate" "${SE_HTTPS_CERTIFICATE}"
   fi
   if [ ! -z "$SE_HTTPS_PRIVATE_KEY" ]; then
-    echo "Appending Selenium options: --https-private-key ${SE_HTTPS_PRIVATE_KEY}"
-    SE_OPTS="$SE_OPTS --https-private-key ${SE_HTTPS_PRIVATE_KEY}"
+    append_se_opts "--https-private-key" "${SE_HTTPS_PRIVATE_KEY}"
   fi
 fi
 
@@ -91,9 +98,13 @@ if [ "$SE_ENABLE_TRACING" = "true" ]; then
     SE_JAVA_OPTS="$SE_JAVA_OPTS ${SE_OTEL_JVM_ARGS}"
   fi
 else
-  SE_OPTS="$SE_OPTS --tracing false"
+  append_se_opts "--tracing" "false"
   SE_JAVA_OPTS="$SE_JAVA_OPTS -Dwebdriver.remote.enableTracing=false"
   echo "Tracing is disabled"
+fi
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "All Selenium options: ${SE_OPTS}"
 fi
 
 java ${JAVA_OPTS:-$SE_JAVA_OPTS} \
